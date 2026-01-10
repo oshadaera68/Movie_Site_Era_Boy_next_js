572e177b73a33eb58a48b32c85381b437b3d5829
oshadaera68 <mindartlk010@gmail.com>
2026-01-10 21:01:39 +0530

    updated

--- data/posts.ts
+++ data/posts.ts
@@ -227,7 +227,7 @@ export const posts = [
     tags: "KalamKaval, KalamKaval Sinhala subtitles, KalamKaval Sinhala Subtitles 2025, era boy",
     excerpt: "KalamKaval movie Sinhala subtitles...",
     content: "2000 අවුරුද්දට කලින් කේරලේ කෝට්ටයිකෝනම් වල වෙන ජන අරගලයක් විමර්ශනය කරන්න එන කේරල පොලිසියේ විශේෂ ශාඛාවේ වැඩ කරන නිලධාරියෙක් වෙන නාත් නොහොත් ජයක්‍රිශ්නන් මේ අරගලය හට ගන්න හේතු පාදක වුනු කරුණ හොයන්න පටන් ගන්නවා. නමුත් මේක ඉවර වෙන්නේ 'පැන ගියා' කියලා හිතන කාන්තාවන් මරණ දාම ඝාතන රැල්ලකින්. (පස්සේ මේක ටිකක් වෙනස් වෙනවා.) මේකේ අතරමැදි කතාව එයා ඒක හොයාගන්නේ කොහොමද වගේම කවුද මේක කරන්නේ කියලා හොයාගන්න එක..",
-    image: "https://i.ytimg.com/vi/LvUnF8oApaM/maxresdefault.jpg",
+    image: "https://i.ytimg.com/vi/UFhZNGedn_Q/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLCpL2Q6kJuQV__jbcZFxv33XSOFyg",
     video: "CamCopy",
     category: "subtitles",
     language: "Malayalam",
